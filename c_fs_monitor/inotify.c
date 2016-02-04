36eb47e0daa0796b35395ee1bd426736489264f4
Ian Kronquist <iankronquist@gmail.com>
2016-02-03 21:09:58 -0800

    Clean up fs monitor c module style & indentation
    I was having trouble figuring out which brackets went where so I decided to
    clean this file up.
    * Fix comment levels.
    * Fix indentation with vim.
    * Strip trailing whitespace.

--- c_fs_monitor/inotify.c
+++ c_fs_monitor/inotify.c
@@ -10,10 +10,10 @@
 
 #define DEBUG 1
 #define JSON_OBJECT "{\"DATE\":\"%s\",\"EVENT\":\"%s\",\"PATH\":\"%s%s\",\"TYPE\":\"%s\"}\n"
-//Maximimum pathname, all escapes and 1 null terminating
-#define PATH_LIMIT PATH_MAX * 2 + 1 
+// Maximimum pathname, all escapes and 1 null terminator
+#define PATH_LIMIT PATH_MAX * 2 + 1
 
-//Defined in limits.h
+// Defined in limits.h
 const char *one         = "IN_ACCESS";
 const char *two         = "IN_MODIFY";
 const char *four        = "IN_ATTRIB";
@@ -30,50 +30,49 @@ const char *file	= "FILE";
 
 static void json_safe(const char * source, char * destination, int size)
 {
-	int l = 0;
-	int k = 0;
-	for( ; k<= size && l < PATH_LIMIT; k++)
-	{
-		if(source[k] == '"' || source[k] == '\n')
-		{
-			destination[l++] = '\\';
-			destination[l++] = source[k];
-		}
-		else
-		{
-			destination[l++] = source[k];
-		}
-	}
+    int l = 0;
+    int k = 0;
+    for( ; k<= size && l < PATH_LIMIT; k++)
+    {
+        if(source[k] == '"' || source[k] == '\n')
+        {
+            destination[l++] = '\\';
+            destination[l++] = source[k];
+        }
+        else
+        {
+            destination[l++] = source[k];
+        }
+    }
 }
+
 /*
  *
  */
 static void print_json(const char * date, const char * event,const char *directory, const char *name, const char *type)
 {
-	char safe_name[NAME_MAX *2 +1];
+    char safe_name[NAME_MAX *2 +1];
 
-	json_safe(name,safe_name,strlen(name));
-	fprintf(stdout,JSON_OBJECT,date,event,directory,safe_name,type);
+    json_safe(name,safe_name,strlen(name));
+    fprintf(stdout,JSON_OBJECT,date,event,directory,safe_name,type);
 }
 
 
 /* Read all available inotify events from the file descriptor 'fd'.
- *          wd is the table of watch descriptors for the directories in argv.
- *                     argc is the length of wd and argv.
- *                               argv is the list of watched directories.
- *                                         Entry 0 of wd and argv is unused. */
-
+ * wd is the table of watch descriptors for the directories in argv.
+ * argc is the length of wd and argv.
+ * argv is the list of watched directories.
+ * Entry 0 of wd and argv is unused. */
 static void
 handle_events(int fd, int *wd, int argc,char *safe_array[])
 {
     /* Some systems cannot read integer variables if they are not
-     *               properly aligned. On other systems, incorrect alignment may
-     *                             decrease performance. Hence, the buffer used for reading from
-     *                                           the inotify file descriptor should have the same alignment as
-     *                                                         struct inotify_event. */
-    
+     * properly aligned. On other systems, incorrect alignment may
+     * decrease performance. Hence, the buffer used for reading from
+     * the inotify file descriptor should have the same alignment as
+     * struct inotify_event. */
     char buf[4096]
-    __attribute__ ((aligned(__alignof__(struct inotify_event))));
+        __attribute__ ((aligned(__alignof__(struct inotify_event))));
     const struct inotify_event *event;
     int i;
     ssize_t len;
@@ -83,41 +82,39 @@ handle_events(int fd, int *wd, int argc,char *safe_array[])
     char *directory = NULL;
     char buffer[80];
     time_t current_time;
-    struct tm * string_time = NULL; 
+    struct tm * string_time = NULL;
 
     current_time = time(&current_time);
-    string_time = localtime(&current_time); 
+    string_time = localtime(&current_time);
     strftime(buffer,80,"%c",string_time);
     /* Loop while events can be read from inotify file descriptor. */
     for (;;)
     {
-        
+
         /* Read some events. */
-        
+
         len = read(fd, buf, sizeof buf);
         if (len == -1 && errno != EAGAIN)
         {
             perror("read");
             exit(EXIT_FAILURE);
         }
-        
+
         /* If the nonblocking read() found no events to read, then
-         *                   it returns -1 with errno set to EAGAIN. In that case,
-         *                                     we exit the loop. */
-        
+         * it returns -1 with errno set to EAGAIN. In that case,
+         * we exit the loop. */
         if (len <= 0)
             break;
-        
+
         /* Loop over all events in the buffer */
-        
         for (ptr = buf; ptr < buf + len;
-             ptr += sizeof(struct inotify_event) + event->len)
+                ptr += sizeof(struct inotify_event) + event->len)
         {
             //fprintf(stdout,"Time: %s",ctime(&current_time));
             event = (const struct inotify_event *) ptr;
-            
+
             /* Print event type */
-            //Fix this section
+            // FIXME: Fix this section
             if (IN_ACCESS & event->mask)
             {
                 mask_ptr = one;
@@ -163,22 +160,22 @@ handle_events(int fd, int *wd, int argc,char *safe_array[])
                 mask_ptr = four_hundo;
             }
             /*if (IN_CLOSE & event->mask)
-            {
-                fprintf(stdout,"IN_CLOSE: ");
-            }
-            if (IN_MOVE & event->mask)
-            {
-                fprintf(stdout,"IN_MOVE: ");
-            }*/
-/*            if(DEBUG)
-            {
-                fprintf(stdout, "UMASK: %X", event->mask);
-            }
-  */      
-	    /* Once DELETE_SELF -> quit
-	    */
+              {
+              fprintf(stdout,"IN_CLOSE: ");
+              }
+              if (IN_MOVE & event->mask)
+              {
+              fprintf(stdout,"IN_MOVE: ");
+              }*/
+            /*            if(DEBUG)
+                          {
+                          fprintf(stdout, "UMASK: %X", event->mask);
+                          }
+             */
+            /* Once DELETE_SELF -> quit
+             */
             /* Print the name of the watched directory */
-            
+
             for (i = 0; i < argc; ++i)
             {
                 if (wd[i] == event->wd)
@@ -187,20 +184,20 @@ handle_events(int fd, int *wd, int argc,char *safe_array[])
                     break;
                 }
             }
-            
+
             /* Print the name of the file */
-            
-//            if (event->len)
-//                fprintf(stdout, "%s", event->name);
+
+            //            if (event->len)
+            //                fprintf(stdout, "%s", event->name);
             /* Print type of filesystem object */
-            
+
             if (event->mask & IN_ISDIR)
-	    {
-		type_ptr = folder;
-	    }
+            {
+                type_ptr = folder;
+            }
             else
             {
-		type_ptr = file;
+                type_ptr = file;
             }
             print_json(buffer,mask_ptr,directory,event->name,type_ptr);
         }
@@ -208,7 +205,7 @@ handle_events(int fd, int *wd, int argc,char *safe_array[])
     fflush(stdout);
 }
 
-int
+    int
 main(int argc, char* argv[])
 {
     char buf;
@@ -217,40 +214,38 @@ main(int argc, char* argv[])
     int *wd;
     nfds_t nfds;
     struct pollfd fds[2];
-    
+
     if (argc < 2)
     {
         fprintf(stdout, "Usage: %s PATH [PATH ...]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
-    
+
+
     /* Create the file descriptor for accessing the inotify API */
-    
+
     fd = inotify_init1(IN_NONBLOCK);
     if (fd == -1)
     {
         perror("inotify_init1");
         exit(EXIT_FAILURE);
     }
-    
+
     /* Allocate memory for watch descriptors */
-    
     wd = calloc(argc, sizeof(int));
     if (wd == NULL)
     {
         perror("calloc");
         exit(EXIT_FAILURE);
     }
-    
+
     /* Mark directories for events
-     *               - file was opened
-     *                             - file was closed */
-    
+     * - file was opened
+     * - file was closed */
     for (i = 1; i < argc; i++)
     {
         wd[i] = inotify_add_watch(fd, argv[i],
-                                  IN_ALL_EVENTS);
+                IN_ALL_EVENTS);
         if (wd[i] == -1)
         {
             fprintf(stderr, "Cannot watch '%s'\n", argv[i]);
@@ -258,26 +253,25 @@ main(int argc, char* argv[])
             exit(EXIT_FAILURE);
         }
     }
-    
+
     /* Prepare for polling */
-    
+
     nfds = 1;
-    
+
     /* Inotify input */
-    
+
     fds[0].fd = fd;
     fds[0].events = POLLIN;
-    
+
     /* Creating JSON safe directory */
     safe_array = malloc((argc) * sizeof(char*));
     for(i = 0,j = 1; i < argc - 1; i++,j++)
     {
-	safe_array[i] = malloc(PATH_LIMIT * sizeof(char));
-	json_safe(argv[j],safe_array[i],strlen(argv[j]));
+        safe_array[i] = malloc(PATH_LIMIT * sizeof(char));
+        json_safe(argv[j],safe_array[i],strlen(argv[j]));
     }
-	
+
     /* Wait for events and/or terminal input */
-    
     fprintf(stderr, "Listening for events.\n");
     while (1)
     {
@@ -289,30 +283,28 @@ main(int argc, char* argv[])
             perror("poll");
             exit(EXIT_FAILURE);
         }
-        
+
         if (poll_num > 0)
         {
 
             if (fds[0].revents & POLLIN)
             {
-                
+
                 /* Inotify events are available */
-                
                 handle_events(fd, wd, argc, safe_array);
             }
         }
     }
-    
+
     fprintf(stdout, "Listening for events stopped.\n");
-    
+
     /* Close inotify file descriptor */
-    
     close(fd);
-    
+
     free(wd);
     for(i = 0; i < argc; i++)
     {
-	free(safe_array[i]);
+        free(safe_array[i]);
     }
     free(safe_array);
 
