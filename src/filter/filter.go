608040574157d53bd724d6b38b57d4503bdc4b74
Ian Kronquist <iankronquist@gmail.com>
2016-02-11 15:23:49 -0800

    Pair program NOP & blacklist filters

--- src/filter/filter.go
+++ src/filter/filter.go
@@ -1,1 +1,58 @@
 package filter
+import (
+	"encoding/json"
+	"strings"
+)
+
+type FilterConfig struct {
+	ignore []string `json:"ignore"`
+}
+
+type Filter interface {
+	Start(c FilterConfig, sending chan<- string, receiving <-chan string)
+}
+
+type FSFilter struct { }
+
+type NOP struct { }
+
+type ZachsInotifyData struct {
+	Date	 string `json:"DATE"`
+	Event	 string `json:"EVENT"`
+	FilePath string `json:"PATH"`
+	Type	 string `json:"TYPE"`
+}
+
+func (f FSFilter) Start (c FilterConfig, sending chan<- string, receiving <-chan string){
+	for {
+		select {
+			case message := <-receiving:
+				zid := ZachsInotifyData{}
+				err := json.Unmarshal([]byte(message), &zid)
+				if err != nil {
+					panic(err)
+				}
+				notblacklisted := true
+				for _, i := range c.ignore {
+					if strings.HasPrefix(zid.FilePath,i) {
+						notblacklisted = false
+						break
+					}
+				}
+				if notblacklisted {
+					sending <- message
+				}
+		}
+	}
+
+}
+
+func (N NOP) Start( c FilterConfig, sending chan<- string, receiving <-chan string){
+	for {
+		select{
+			case message := <- receiving:
+				sending <- message
+		}
+	}
+
+}
