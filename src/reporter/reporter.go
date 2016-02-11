db10b99bacdb649f0e23762a5d0464ad0a570c61
Ian Kronquist <iankronquist@gmail.com>
2016-02-11 15:53:44 -0800

    Add basic reporter implementation

--- src/reporter/reporter.go
+++ src/reporter/reporter.go
@@ -1,1 +1,15 @@
 package reporter
+
+import (
+	"github.com/fw42/go-hpfeeds"
+)
+
+func StartReporter(host string, port int, ident, auth string, channels []chan []byte) {
+	hp := hpfeeds.NewHpfeeds(host, port, ident, auth)
+	hp.Log = true
+	hp.Connect()
+	for _, channel := range channels {
+		hp.Publish("", channel)
+	}
+
+}
