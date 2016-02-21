26b33bad2e595f7e990ede566dccbf461b32324d
pixel5919 <reboot5919@yahoo.com>
2016-02-21 00:53:45 -0800

    Started new file monitor with options

---
+++ c_fs_monitor/znotify.c
@@ -0,0 +1,92 @@
+#include <errno.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "znotify.h"
+
+#define OPT_W 0
+#define OPT_T 1
+#define OPT_A 2
+#define OPT_N 3
+
+// Defined in limits.h
+const char *in_access_cstring           = "IN_ACCESS";
+const char *in_modify_cstring           = "IN_MODIFY";
+const char *in_attrib_cstring           = "IN_ATTRIB";
+const char *in_close_write_cstring      = "IN_CLOSE_WRITE";
+const char *in_close_no_write_cstring   = "IN_CLOSE_NOWRITE";
+const char *in_open_cstring             = "IN_OPEN";
+const char *in_moved_from_cstring       = "IN_MOVED_FROM";
+const char *in_moved_to_cstring         = "IN_MOVED_TO";
+const char *in_create_cstring           = "IN_CREATE";
+const char *in_delete_cstring           = "IN_DELETE";
+const char *in_delete_self_cstring      = "IN_DELETE_SELF";
+const char *folder_cstring              = "FOLDER";
+const char *file_cstring                = "FILE";
+
+static void help_menu(char* file);
+
+int main(int argc, char* argv[])
+{
+	int i;
+	short options[4] = {0,0,0,0};
+	
+	while( (i = getopt(argc, argv, "w:t:han")) != -1)
+	{
+		switch(i)
+		{
+			case 'w':
+				options[OPT_W] = 1;
+				break;
+			case 't':
+				options[OPT_T] = 1;
+				break;
+			case 'h':
+				help_menu(argv[0]);
+				exit(EXIT_SUCCESS);			
+				break;
+			case 'a':
+				options[OPT_A] = 1;
+				break;
+			case 'n':
+				options[OPT_N] = 1;
+				break;
+			case ':':
+				fprintf(stderr,"Missing Directory\n");
+				exit(EXIT_FAILURE);
+				break;
+			case '?':
+				fprintf(stderr,"Use: %s -h\n",argv[0]);
+				exit(EXIT_FAILURE);
+				break;
+			default:
+				fprintf(stderr,"\nInvalid Choice: %s -h",argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
+
+/*************************************************************************
+ * Function: 	help_menu
+ * Description: Prints out commands for help
+ * Parameters:  verbose for debugging
+ * Pre-Conditions: None
+ * Post-Conditions: Prints help
+*************************************************************************/
+static void help_menu(char * file)
+{
+	fprintf(stderr,"\nGeneral: %s [w or t] [a or n] [directories to be watched]",file);
+	fprintf(stderr,"\n\t-w Watch Only, do NOT Traverse");
+	fprintf(stderr,"\n\t-t Traverse and Add Child Directories to Watch List");
+	fprintf(stderr,"\n\t-h Display this Help Menu");
+	fprintf(stderr,"\n\t-a Add Created Child Directories to Watch List");
+	fprintf(stderr,"\n\t-n Watch for Only Creation Events [DEFAULT]\n");
+	return;
+}
---
+++ c_fs_monitor/znotify.h
@@ -0,0 +1,108 @@
+/*
+ *
+ *
+ *
+ */ 
+#ifndef __ZNOTIFY_H_
+#define __ZNOTIFY_H_
+
+#include <limits.h>
+
+/* This is to be put a fprintf statement with 5 strings. The path is 
+ * represented by two strings, directory + file or directory name
+ */ 
+ #define JSON_OBJECT "{\"DATE\":\"%s\",\"EVENT\":\"%s\",\"PATH\":\"%s%s\",\"T    YPE\":\"%s\"}\n"
+
+#define PATH_LIMIT PATH_MAX * 2 + 1
+
+#define TIME_OUTPUT "%c"
+
+/* Takes two c strings and copies source into destination string
+ * inserting JSON escape sequences to make it JSON safe
+ */ 
+void json_safe(const char * source, char * destination, int size)
+/*****************************************************************************
+ * Function: 
+ * Description:
+ * Parameters: 
+ * Pre-Conditions:
+ * Post-Conditions: 
+ ****************************************************************************/
+{
+    int l = 0;
+    int k = 0;
+    for( ; k< size && l < PATH_LIMIT; k++)
+    {
+        if(source[k] == '"' || source[k] == '\n' || source[k] == '\'' || source[k] == '\t')
+        {
+            destination[l++] = '\\';
+            destination[l++] = source[k];
+        }
+        else
+        {
+            destination[l++] = source[k];
+        }
+    }
+	/* Just to be sure it's null terminated */
+	if(l < PATH_LIMIT)
+	{
+		destination[l] = '\0';
+	}
+	else
+	{
+		destination[l - 1] = '\0';
+	}
+}
+
+/* Print a JSON object as defined by JSON_OBJECT
+ * Takes c style strings only, takes JSON safe directory name
+ * Takes unsafe file name and makes it safe.
+ */
+
+/*****************************************************************************
+ * Function: 
+ * Description:
+ * Parameters: 
+ * Pre-Conditions:
+ * Post-Conditions: 
+ ****************************************************************************/
+void print_json(const char * date, const char * event,const char *directory, const char *name, const char *type)
+{
+    char safe_name[NAME_MAX *2 +1];
+    
+    json_safe(name,safe_name,strlen(name));
+    
+    fprintf(stdout,JSON_OBJECT,date,event,directory,safe_name,type);
+}
+
+/*************************************************************************
+ * Function: fetch_argument
+ * Description: Takes a dependent i and all of command line arguments
+ * 		purpose is to return the next command in the command line
+ * 		that is not an argument
+ * Parameters: Pointer i for index, command line and string address for 
+ * 	       return
+ * Pre-Conditions: i is exists, char exists
+ * Post-Conditions: i is incremented and returns with new argument
+*************************************************************************/
+void fetch_argument(int *i, int argc, char *argv[], char **voltar)
+{
+	for((*i)++;*i < argc; (*i)++)
+	{
+		if(argv[*i][0] != '-')
+		{
+			*voltar = argv[*i];
+			return;
+		}
+	}
+	*voltar = NULL;
+return;
+}
+/*****************************************************************************
+ * Function: 
+ * Description:
+ * Parameters: 
+ * Pre-Conditions:
+ * Post-Conditions: 
+ ****************************************************************************/
+#endif
