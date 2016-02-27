aced8cedac4c150a68aac8b1d51bac25012f582b
Ian Kronquist <iankronquist@gmail.com>
2016-02-26 17:49:12 -0800

    Rename StartReporter to Start
    reporter.StartReporter is redundant

--- src/reporter/reporter.go
+++ src/reporter/reporter.go
@@ -4,12 +4,11 @@ import (
 	"github.com/fw42/go-hpfeeds"
 )
 
-func StartReporter(host string, port int, ident, auth string, channels []chan []byte) {
+func Start(host string, port int, ident, auth string, channels []chan []byte) {
 	hp := hpfeeds.NewHpfeeds(host, port, ident, auth)
 	hp.Log = true
 	hp.Connect()
 	for _, channel := range channels {
 		hp.Publish("", channel)
 	}
-
 }
