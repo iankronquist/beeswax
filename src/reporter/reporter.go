403dbf801aab4e30bed28ad3cd87dbfece86f1ce
Ian Kronquist <iankronquist@gmail.com>
2016-03-31 21:21:25 -0700

    Fix reporter type signature
    Also add a helper function from a golang blogpost on pipelines.
    http://blog.golang.org/pipelines

--- src/reporter/reporter.go
+++ src/reporter/reporter.go
@@ -1,17 +1,41 @@
 package reporter
 
-import(
-	"github.com/fw42/go-hpfeeds"
+import (
 	"fmt"
+	"github.com/fw42/go-hpfeeds"
+	"sync"
 	"time"
 )
 
-func StartReporter() {
+// Taken from http://blog.golang.org/pipelines
+// Modified to support chan []byte instead of <-chan int
+func merge(cs ...chan []byte) chan []byte {
+	var wg sync.WaitGroup
+	out := make(chan []byte)
 
-	host := "http://159.203.253.60"
-	port := 20000
-	ident := "1ca0ec5a-eb51-11e5-a612-0401af4dc501"
-	auth := "L1uUI9Tw"
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan []byte) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func Start(host string, port int, ident string, auth string, outputs []chan []byte) {
 
 	hp := hpfeeds.NewHpfeeds(host, port, ident, auth)
 	hp.Log = true
@@ -19,7 +43,7 @@ func StartReporter() {
 
 	// Publish something on "flotest" every second
 	// Channel1 is where we put the filtered JSON data
-	channel1 := make(chan []byte)
+	channel1 := merge(outputs...)
 	hp.Publish("beeswax.events", channel1)
 	go func() {
 		for {
