d3c39d529f115f5a16cfd3c3a8ee907d7c66bc0c
Ian Kronquist <iankronquist@gmail.com>
2016-04-14 14:18:30 -0700

    First pass at a deployment script

---
+++ deploy_beeswax.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+SERVER_URL=$1
+DEPLOY_KEY=$2
+
+INSTALL_PATH=/opt/beeswax_gopath/src/
+HONEYPOT_NAME=senior-project-experiment
+MHN_PATH=/opt/mhn/
+
+
+install_dependencies() {
+	apt-get update
+	apt-get -y install docker tcpdump gcc golang git supervisor python-pip
+	pip install docker-compose
+}
+
+install_project() {
+	mkdir -p $INSTALL_PATH
+	git clone http://github.com/iankronquist/senior-project-experiment.git $INSTALL_PATH/$HONEYPOT_NAME
+	cd $INSTALL_PATH/$HONEYPOT_NAME
+	make all
+}
+
+# The docker group must already exist
+# The project must already be cloned
+make_user() {
+	useradd -d /home/beeswax -s /bin/bash -m beeswax -g users -G docker
+	chown -R beeswax $INSTALL_PATH
+}
+
+make_configs() {
+	source $MHN_PATH/scripts/registration.sh $SERVER_URL $DEPLOY_KEY "beeswax"
+	cat  > $INSTALL_PATH/$HONEYPOT_NAME/honeypot_config.json <<EOF
+{
+  "monitor process name": "c_fs_monitor/znotify",
+  "docker compose name": "docker-compose",
+  "container names": ["mysql", "wordpress"],
+	"mhn host": "$HPF_HOST",
+	"mhn port": $HPF_PORT,
+	"mhn identifier": "$HPF_IDENT",
+	"mhn authorization": "$HPF_SECRET"
+}
+EOF
+	cp $INSTALL_PATH/$HONEYPOT_NAME/beeswaf_supervisor.conf /etc/supervisor/conf.d/beeswax.conf
+}
+
+# The project must already be cloned
+enable_services() {
+	# Ensure docker is enabled and started
+	systemctl enable docker
+	systemctl start docker
+
+	supervisorctl update
+}
+
+
+install_dependencies
+install_project
+make_user
+make_configs
+enable_services
