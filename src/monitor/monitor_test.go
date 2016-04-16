4a25a7fd4ba38ae83989bf6daf609334cc46ae23
pixel5919 <reboot5919@yahoo.com>
2016-04-16 01:04:32 +0000

    More Test

--- src/monitor/monitor_test.go
+++ src/monitor/monitor_test.go
@@ -2,6 +2,7 @@ package monitor
 
 import "testing"
 import "reflect"
+//import "strings"
 
 type testCommandData struct{
 	command	  string
@@ -21,6 +22,24 @@ func TestRunCommandAndSlurpOutput( t *testing.T) {
 		}
 		if !reflect.DeepEqual(actual, item.expected) {
 			t.Error("Expected Nothing but Got:",actual)	
+		}
+	}
+}
+
+func TestRunCommandAndChannelOutput( t *testing.T) {
+		
+	testArray := []testCommandData{{"echo",[]string{},[]string{}},
+				{"echo",[]string{"hello"},[]string{"hello"}},
+				{"echo",[]string{"-e","Hello\nWorld"},[]string{"Hello","World"}}}
+	for _, item := range(testArray) {
+		c := make( chan []byte)
+		go runCommandAndChannelOutput(item.command,item.arguments,c)
+		i := 0
+		for instance := range c {
+			if item.expected[i] != string(instance) {
+				t.Errorf("Expected \"%s\" but got \"%s\"",item.expected[i],string(instance))
+			}
+			i++
 		}
 	}
 }
